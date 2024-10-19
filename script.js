bc5da1ea4cee4fb2580b5f95aa882256d1e5ec04
ATinyShark <langenbucher.marcell@gmail.com>
2024-10-19 17:33:36 +0200

    New font and improved pixel art.

--- script.js
+++ script.js
@@ -4,14 +4,45 @@ const context = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
 
+const scaleFactor = 2;
+
+const loadAsset = (path) => {
+    const asset = {};
+    
+    asset.image = new Image();
+    asset.image.onload = () => {
+        asset.width = asset.image.width / scaleFactor;
+        asset.height = asset.image.height / scaleFactor;
+    };
+    asset.image.src = path;
+    
+    return asset;
+}
+
+const dinoAsset = loadAsset("assets/dino/dino.png");
+const dinoRunning1Asset = loadAsset("assets/dino/dino_running_1.png");
+const dinoRunning2Asset = loadAsset("assets/dino/dino_running_2.png");
+
 let y = 0;
 let yVelocity = 0;
 let currentRunningState = 0;
 let score = 0;
 let highScore = 0;
 
-function render() {
-    currentRunningState = !currentRunningState;
+let lastTime = Date.now();
+let lastRunningPhaseSwitch = lastTime;
+
+const renderScene = () => {
+    let currentTime = Date.now();;
+    let deltaTime = currentTime - lastTime;
+    lastTime = currentTime;
+
+    if(lastRunningPhaseSwitch + 100 < currentTime) {
+        currentRunningState = !currentRunningState;
+
+        lastRunningPhaseSwitch = currentTime;
+    }
+
     
     context.clearRect(0, 0, width, height);
 
@@ -21,7 +52,7 @@ function render() {
         y = 0;
     }
 
-    score++;
+    score += deltaTime * 0.1;
 
     context.moveTo(0, height - 50);
     context.lineTo(width, height - 50);
@@ -29,21 +60,22 @@ function render() {
     context.stroke();
 
     if(y > 0) {
-        context.drawImage(document.getElementById("dino-texture"), 100, height - 75 - y);
+        context.drawImage(dinoAsset.image, 100, height - 150 - y, dinoAsset.width, dinoAsset.height);
     } else {
         if(currentRunningState) {
-            context.drawImage(document.getElementById("dino-running-1-texture"), 100, height - 75 - y);
+            context.drawImage(dinoRunning1Asset.image, 100, height - 150 - y, dinoRunning1Asset.width, dinoRunning1Asset.height);
         } else {
-            context.drawImage(document.getElementById("dino-running-2-texture"), 100, height - 75 - y);
+            context.drawImage(dinoRunning2Asset.image, 100, height - 150 - y, dinoRunning2Asset.width, dinoRunning2Asset.height);
         }
     }
 
+    context.font = "32px PixelArtFont";
+
     context.textAlign = "right";
     context.textBaseline = "top";
-    context.fillText(score, width - 5, 5);
-    context.fillText(highScore, width - 10, 5);
+    context.fillText(Math.floor(score), width - 5, 5);
 
-    requestAnimationFrame(render);
+    requestAnimationFrame(renderScene);
 }
 
 document.addEventListener("keydown", (e) => {
@@ -54,5 +86,5 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
-render();
+renderScene();
 
